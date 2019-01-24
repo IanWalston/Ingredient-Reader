61f5a57e8306dcb741ff41458be3b0759dc9dfe6
Ian Walston <ikwalston@comcast.net>
2019-01-23 22:25:35 -0500

    heroku still dont work

--- routes.js
+++ routes.js
@@ -3,7 +3,10 @@ module.exports = app => {
   const path = require("path");
   const recognize = require(path.join(__dirname, "/control/recognize.js"));
   const imageupload = require(path.join(__dirname, "/control/imageupload.js"));
-  const ingredientfilter = require(path.join(__dirname, "/control/ingredientfilter.js"));
+  const ingredientfilter = require(path.join(
+    __dirname,
+    "/control/ingredientfilter.js"
+  ));
 
   const multer = require("multer");
   const upload = multer({
@@ -11,25 +14,31 @@ module.exports = app => {
     // you might also want to set some limits: https://github.com/expressjs/multer#limits
   });
 
-  app.get('/', (req, res)=>{
-    res.render("index", { ingredients: ['your ingredients will be displayed here'] });
-  })
+  app.get("/", (req, res) => {
+    res.render("index", {
+      ingredients: ["your ingredients will be displayed here"]
+    });
+  });
 
   app.post(
-    "/upload",
+    path.join(__dirname, "/upload"),
     upload.single("file" /* name attribute of <file> element in your form */),
     (req, res) => {
-      imageupload(req, res).then(() => {
-        recognize().then(data => {
-          console.log(data);
+      imageupload(req, res)
+        .then(() => {
+          recognize().then(data => {
+            console.log(data);
 
-          const ingredientarr = ingredientfilter(data)
+            const ingredientarr = ingredientfilter(data);
 
-          res
-            .status(200) //OK
-            .render("index", { ingredients: ingredientarr });
+            res
+              .status(200) //OK
+              .render("index", { ingredients: ingredientarr });
+          });
+        })
+        .catch(err => {
+          throw err;
         });
-      }).catch(err=>{throw(err)});
     }
   );
 };
